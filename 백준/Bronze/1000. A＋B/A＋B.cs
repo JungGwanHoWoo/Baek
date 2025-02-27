7573c94149dba17d7f847fede00e26b13243daa9
JungGwanHoWoo <gwanhoj931@gmail.com>
2025-02-27 19:41:12 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,19 @@
+using System;
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+    
+            string s = Console.ReadLine();
+            string[] sl = s.Split(" ");
+
+            a = int.Parse(sl[0]);
+            b = int.Parse(sl[1]);
+    
+            Console.WriteLine (a + b);
+        }
+    }
+}
