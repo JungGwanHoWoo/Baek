0269675e01f085962a830e74940fd86f428fc169
JungGwanHoWoo <gwanhoj931@gmail.com>
2025-03-04 17:45:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,17 @@
+
+using System;
+using System.Text;
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            char s = Convert.ToChar(Console.ReadLine());
+            int result = Convert.ToInt32(s);
+            System.Console.WriteLine(result);
+      
+        }
+    }
+}
