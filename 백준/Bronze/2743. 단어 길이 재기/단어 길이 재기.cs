99ec77e3d2f1003f6b7ddb0e41c87caef45bf5b3
JungGwanHoWoo <gwanhoj931@gmail.com>
2025-03-04 17:47:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Text;
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string s = Console.ReadLine();
+            System.Console.WriteLine(s.Length);
+         
+        }
+    }
+}
