523c4e66965b81baf8ad09ae243e23b3d4491cfe
JungGwanHoWoo <gwanhoj931@gmail.com>
2025-03-06 20:03:26 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
@@ -1,23 +1,6 @@
+string[] s = Console.ReadLine().Split();
+int A = int.Parse(s[0]);
+int B = int.Parse(s[1]);
+int V = int.Parse(s[2]);
 
-using System;
-using System.Text;
-namespace 백준
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            string[] s = Console.ReadLine().Split();
-
-            double s1 = double.Parse(s[0]);
-            double s2 = double.Parse(s[1]);
-            double s3 = double.Parse(s[2]);
-
-            double i = 1;
-            i += Math.Ceiling((s3 -s1) / (s1-s2));
-            System.Console.WriteLine((int)i);
-       
-        }
-    }
-}
+Console.Write(Math.Ceiling((double)(V-B)/ (A - B)));
